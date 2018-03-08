ffcf57ebaaccb3229e6395289f93bdc9a610c7f2
Student <uauser@lab13.uantwerpen.be>
2018-03-08 14:41:10 +0100

    .

---
+++ cmake-build-debug/CMakeFiles/Luchthaven.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/uauser/CLionProjects/Luchthaven/main.cpp" "/home/uauser/CLionProjects/Luchthaven/cmake-build-debug/CMakeFiles/Luchthaven.dir/main.cpp.o"
+  "/home/uauser/CLionProjects/Luchthaven/tinystr.cpp" "/home/uauser/CLionProjects/Luchthaven/cmake-build-debug/CMakeFiles/Luchthaven.dir/tinystr.cpp.o"
+  "/home/uauser/CLionProjects/Luchthaven/tinyxml.cpp" "/home/uauser/CLionProjects/Luchthaven/cmake-build-debug/CMakeFiles/Luchthaven.dir/tinyxml.cpp.o"
+  "/home/uauser/CLionProjects/Luchthaven/tinyxmlerror.cpp" "/home/uauser/CLionProjects/Luchthaven/cmake-build-debug/CMakeFiles/Luchthaven.dir/tinyxmlerror.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Luchthaven.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Luchthaven.dir/main.cpp.o"
+  "CMakeFiles/Luchthaven.dir/tinystr.cpp.o"
+  "CMakeFiles/Luchthaven.dir/tinyxml.cpp.o"
+  "CMakeFiles/Luchthaven.dir/tinyxmlerror.cpp.o"
+  "Luchthaven.pdb"
+  "Luchthaven"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Luchthaven.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
